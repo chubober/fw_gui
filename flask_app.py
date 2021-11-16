551d5b5a0489da96ea0451e0c20d30582ecc395a
DariaRev <dr.ea.r@ya.ru>
2021-11-16 21:01:45 +0300

    hello

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from search import find_evth
+from dl_insert_data import main, save_to_db
 # import gspread
 from sqlalchemy import func
 from sqlalchemy import create_engine
@@ -8,11 +10,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from ordered_set import OrderedSet as ordset
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 # from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
-engine = create_engine('postgresql+psycopg2://lingvist:lingvistpassword@127.0.0.1:5432/mydatabase')
+engine = create_engine('postgresql+psycopg2://lingvist:lingvistpassword@178.154.193.115:5432/mydatabase')
 
 Base = declarative_base()
 
@@ -53,26 +56,26 @@ def chunker(req_dict, n):
     return req_dicts
 
 
-def search_by_parameters(db_session, req_dicts):
-    res_set = ordset()
-    for d in req_dicts:
-        # all_texts = list(set(x.text for x in db_session.query(DF).all() if x))
-        # if not texts:
-        #     texts = all_texts
+# def search_by_parameters(db_session, req_dicts):
+#     res_set = ordset()
+#     for d in req_dicts:
+#         # all_texts = list(set(x.text for x in db_session.query(DF).all() if x))
+#         # if not texts:
+#         #     texts = all_texts
 
-        new_query = db_session.query(DF)
+#         new_query = db_session.query(DF)
 
-        records = new_query.filter(DF.text.in_(d['source']),
-                                  func.lower(DF.clause).contains(d['word']),
-                                  func.lower(DF.tr).contains(d['trans']),
-                                  DF.subj.in_(d['subj']),
-                                  DF.obj.in_(d['obj']),
-                                  DF.verb.in_(d['verb']),
-                                  DF.wo.in_(d['wo']),
-                                  ).all()
+#         records = new_query.filter(DF.text.in_(d['source']),
+#                                   func.lower(DF.clause).contains(d['word']),
+#                                   func.lower(DF.tr).contains(d['trans']),
+#                                   DF.subj.in_(d['subj']),
+#                                   DF.obj.in_(d['obj']),
+#                                   DF.verb.in_(d['verb']),
+#                                   DF.wo.in_(d['wo']),
+#                                   ).all()
 
-        res_set.update(list(records))
-    return list(res_set)
+#         res_set.update(list(records))
+#     return list(res_set)
 
 
 def dicter(records):
@@ -93,6 +96,7 @@ def dicter(records):
 
 @app.route('/')
 def main_page():
+    
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
@@ -109,26 +113,50 @@ def main_page():
                            subjs=subjs, objs=objs,
                            verbs=verbs, wos=wos)
 
+    
+    return render_template('index.html')
 
 @app.route('/result', methods=['get'])
 def result():
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Base.metadata.create_all(engine)
+    # Session = sessionmaker(bind=engine)
+    # session = Session()
 
     req_dict = request.args.to_dict(flat=False)
     req_dicts = chunker(req_dict, 7)
 
-    results = search_by_parameters(session, req_dicts)
+    # results = search_by_parameters(session, req_dicts)
+    # session.close()
 
-    session.close()
+    print(req_dicts)
+
+    results = find_evth(req_dicts)
 
     res_dicts = dicter(results)
 
     return render_template('result.html', results=res_dicts,
                            isinst=isinstance, lst=list)
 
+@app.route('/upload')
+def upload():
+    return (render_template('upload.html'))
+
+@app.route('/res_corp', methods=['post'])
+def res_corp():
+    if request.method == 'POST':
+        file = request.files['filename']
+    if file:
+        filename = secure_filename(file.filename)
+        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #a = 'file uploaded'
+        file.save(file.filename)
+        #test = pd.read_excel(filename, sheet_name = "Лист1",  na_values = "", keep_default_na = False)
+        res = main(file.filename)
+        
+
+    return redirect(url_for('main_page'))
+    #render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
