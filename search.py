edd34ccc4f3bf0710fb4d0461650003fc3557242
Daria <32986053+DariaRev@users.noreply.github.com>
2021-11-16 13:14:44 +0300

    Update search.py

--- search.py
+++ search.py
@@ -8,7 +8,7 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # from sqlalchemy import create_engine
-
+'''
 cards = [{
         "word": "щас|r'izan'-cə|er'a-tamə|.",
         "trans": "",
@@ -23,18 +23,12 @@ cards = [{
         "subj": ['pro-1', 'ns', 'pro1pl'], "obj": "", "verb": ""
     }]
 
+'''
 
 def find_evth(cards):
   '''
   Поиск по карточкам
   '''
-  connection = psycopg2.connect(user="lingvist",
-                                    password="lingvistpassword",
-                                    host="178.154.193.115",
-                                    port="5432",
-                                    database="mydatabase")
-
-  cursor = connection.cursor()
   query = f"""select * from data """
 
   for card_ix, card in enumerate(cards):
@@ -43,19 +37,19 @@ def find_evth(cards):
     for key, value in card.items():
       if value == "":
         continue
-      if key == 'word' or key == 'trans':
-        new_val = "lower("+key+")" + " like lower('%" + value.replace("'", "''") + "%')"
+      if key == 'clause' or key == 'tr':
+        new_val = "lower("+key+")" + " like lower('%" + replace_quot_sql(value) + "%')"
       elif type(value) is list:
         new_val = ""
         for elem_ix, elem in enumerate(value):
           if new_val != "":
             new_val += "or "
-          new_val += key + f" = '{elem}' "
+          new_val += key + f" = '{replace_quot_sql(elem)}' "
         if new_val != "":
           new_val ="("+new_val+ ")"
           #where += l_val
       else:
-        new_val = key + f"= '{value}'"
+        new_val = key + "= '"+replace_quot_sql(value)+"'"
       if new_val != "":
         if where != "":
           where += " and "
@@ -75,5 +69,5 @@ def find_evth(cards):
   connection.close()
   return res
 
-
-find_evth(cards)
+def replace_quot_sql(t):
+  return str(t.replace("'", "''"))
