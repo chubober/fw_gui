2fbe3ff0f2090188fefcf683ada75c760f582ec2
DariaRev <dr.ea.r@ya.ru>
2022-04-01 17:48:09 +0300

    new_sql

--- search.py
+++ search.py
@@ -10,8 +10,8 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 # from sqlalchemy import create_engine
 
 cards = [{
-        'clause': '', 
-        'tr': 'есть', 
+        'clause': 'sss or ghjt', 
+        'tr': 'я or он and они', 
         'text': ['AET_AK_inteview_2013', 'LVJ_AK_280713_oryol_i_mysh', 'TAM_IE_04072014_housekeeping'], 
         'subj': ['pro-1'], 
         'obj': ['ns'], 
@@ -43,7 +43,16 @@ def find_evth(cards, corp_id):
       if value == "":
         continue
       if type(value) is str:
-        new_val = "lower("+key+")" + " like lower('%%" + replace_quot_sql(value) + "%%')"
+        new_liv = value.split(' ')
+        #print(new_liv)
+        new_val = ""
+        for word in new_liv:
+          if word == "or" or word == "and":
+            new_val += word + " "
+            continue
+          new_val += "lower("+key+")" + " like lower('%%" + replace_quot_sql(word) + "%%') "
+        #print(new_val)
+        #new_val += "lower("+key+")" + " like lower('%%" + replace_quot_sql(value) + "%%')"
       elif type(value) is list:
         new_val = ""
         for elem_ix, elem in enumerate(value):
@@ -66,10 +75,10 @@ def find_evth(cards, corp_id):
     if where != "":
       query += where
 
-
+  print(query)
   return query
 
 def replace_quot_sql(t):
   return str(t.replace("'", "''"))
 
-#print(find_evth(cards))
+#print(find_evth(cards, 1))
